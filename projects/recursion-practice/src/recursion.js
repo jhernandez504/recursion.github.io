506ee8a951450d7e416aa86b7ed98df08b5d1a12
jhernandez504 <jeremy.hernandez504@gmail.com>
2024-06-13 16:31:38 -0500

    finished compress()

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -416,7 +416,16 @@ var letterTally = function(str, output={}) {
 // elements should not be changed.
 // Example: compress([1, 2, 2, 3, 4, 4, 5, 5, 5]) // [1, 2, 3, 4, 5]
 // Example: compress([1, 2, 2, 3, 4, 4, 2, 5, 5, 5, 4, 4]) // [1, 2, 3, 4, 2, 5, 4]
-var compress = function(list) {
+var compress = function(list, output=[]) {
+  if(list.length === 0){
+    return output;
+  }
+  //tests if output array is empty or current index isnt equal to last index of output array
+  if(output.length === 0 || output[output.length - 1] !== list[0]){
+    output.push(list[0]);
+  }
+ 
+  return compress(list.slice(1), output);
 };
 
 // 32. Augment every element in a list with a new value where each element is an array
